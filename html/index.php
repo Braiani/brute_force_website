79318e6d92d6e2d41774b26d624863dd370e2b2c
Braiani <felipe.braianitech@gmail.com>
2024-05-19 18:28:25 -0400

    Changed how login functions works to be exploitable by hydra

--- html/index.php
+++ html/index.php
@@ -10,7 +10,42 @@ if (!isset($_SESSION['login_attempts'])) {
     $_SESSION['login_attempts'] = 0;
 }
 
-$show_captcha = $_SESSION['login_attempts'] >= 3;
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+$captcha = $_POST['captcha'] ?? '';
+$logins_attempts = 3;
+
+$valid_user = 'abaldeiro';
+$valid_pass = 'password';
+
+$_SESSION['login_attempts'] += 1;
+
+if ($_SESSION['login_attempts'] > $logins_attempts) {
+    $captcha_num1 = $_SESSION['captcha_num1'];
+    $captcha_num2 = $_SESSION['captcha_num2'];
+    $captcha_result = $captcha_num1 + $captcha_num2;
+
+    if ($captcha != $captcha_result) {
+        $_SESSION['captcha_num1'] = rand(1, 9);
+        $_SESSION['captcha_num2'] = rand(1, 9);
+        
+        $_SESSION['error'] = "Invalid captcha.";
+    }
+}
+
+$_SESSION['captcha_num1'] = rand(1, 9);
+$_SESSION['captcha_num2'] = rand(1, 9);
+
+if ($username !== $valid_user){
+    $_SESSION['error'] = "Invalid username.";
+}elseif ($password !== $valid_pass){
+    $_SESSION['error'] = "Invalid password.";
+}else{
+    $_SESSION['logged_in'] = true;
+    $_SESSION['login_attempts'] = 0;
+}
+
+$show_captcha = $_SESSION['login_attempts'] >= $logins_attempts;
 $hasAlert = isset($_SESSION['error']);
 ?>
 
@@ -29,7 +64,7 @@ $hasAlert = isset($_SESSION['error']);
     <?php unset($_SESSION['error']); ?>
     <?php endif; ?>
 
-    <form action="login.php" method="post">
+    <form action="/" method="post">
         <h2>Login</h2>
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required autofocus>
